3b738bbedc86772e7089ad917a5cbd13a54e21d3
chuga <chugainov09032004@yandex.ru>
2026-07-30 16:45:39 +0500

    Возврат словарей в интерфейсе

--- MusicManager/interface.py
+++ MusicManager/interface.py
@@ -1,21 +1,23 @@
 from Database import Database
 from MusicManager import MusicManager
 
-sql = Database().sql_connect()
+def sql():
+    return Database().sql_connect()
 
-def normalise_track(track_dict):
+def _normalise_track(track_dict):
     keys = ['id', 'title', 'author', 'album', 'year']
     return {key: track_dict[key] for key in keys}
 
 def get_track_by_id(id):
-    cursor = sql.cursor()
+    cursor = sql().cursor()
     cursor.execute("""
     SELECT * FROM tracks WHERE id = ?
     """, (id,))
-    return cursor.fetchone()
+    row = cursor.fetchone()
+    return dict(row) if row else None
 
 def get_tracks_by_album(album_name, author=None):
-    cursor = sql.cursor()
+    cursor = sql().cursor()
     if author is None:
         cursor.execute("""
         SELECT * FROM tracks WHERE album = ? ORDER BY title
@@ -24,17 +26,18 @@ def get_tracks_by_album(album_name, author=None):
         cursor.execute("""
         SELECT * FROM tracks WHERE album = ? AND author = ? ORDER BY title
         """, (album_name, author, ))
-    return cursor.fetchall()
+    return [dict(row) for row in cursor.fetchall()]
 
 def get_tracks_by_author(author):
-    cursor = sql.cursor()
+    cursor = sql().cursor()
     cursor.execute("""
     SELECT * FROM tracks WHERE author = ? ORDER BY title
     """, (author,))
+    return [dict(row) for row in cursor.fetchall()]
 
 def search_tracks(query):
     """Ищет каждое слово в полях: title, author, album, year"""
-    cursor = sql.cursor()
+    cursor = sql().cursor()
 
     if not query or query.strip() == '':
         return []
@@ -49,11 +52,9 @@ def search_tracks(query):
     params = []
 
     for word in words:
-        # Дополнительная проверка: слово не должно быть пустым и должно содержать только безопасные символы
         if not word or len(word.strip()) == 0:
             continue
 
-        # "Экранируем" спецсимволы SQL LIKE (%, _) во избежание инъекций
         safe_word = word.replace('%', '\\%').replace('_', '\\_')
 
         condition = """
@@ -76,28 +77,30 @@ def search_tracks(query):
     """
 
     cursor.execute(query_sql, params)
-    return cursor.fetchall()
+    return [dict(row) for row in cursor.fetchall()]
 
-def organize_files(printLogs=False):
+def _organize_files(printLogs=False):
     """Файлы музыки будут автоматом организованы в правильную папку и указаны в БД"""
     manager = MusicManager()
     manager.organize_files(printLogs)
 
-def organize_single_file(file, printLogs=False):
+def add_music_file(file, printLogs=False):
     """Один конкретный файл организует по папкам и добавляет в БД"""
     manager = MusicManager()
-    return manager.organize_single_file(file, printLogs)
+    return manager.add_music_file(file, printLogs)
 
 def delete_track_by_id(track_id, print_logs=True):
     manager = MusicManager()
     return manager.delete_track(track_id, print_logs)
 
 if __name__ == "__main__":
-    results = get_track_by_id(35)
+    result = ""
 
     try:
-        if results:
-            for row in results:
+        if result:
+            for row in result:
                 print(dict(row))
-        else: print(results)
-    except TypeError: print(dict(results))
+        else:
+            print(result)
+    except TypeError:
+        print(dict(result))
